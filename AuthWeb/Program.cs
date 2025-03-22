ec0c5eb0048df40e250d43a376384266a11cb4cb
trokhin87 <trokhin87@gmail.com>
2025-03-23 00:17:31 +0300

    делаю програм нихуя полка

--- AuthWeb/Program.cs
+++ AuthWeb/Program.cs
@@ -1,3 +1,4 @@
+using Interfaces;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,7 +10,21 @@ Log.Logger = new LoggerConfiguration()
     .CreateLogger();
 
 builder.Host.UseSerilog();
+builder.Services.AddControllers();
+builder.Services.AddScoped<IJwtService, JwtService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+
+builder.Services.AddHttpClient();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 app.UseSerilogRequestLogging();
 app.Run();
